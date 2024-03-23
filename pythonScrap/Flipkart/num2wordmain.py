86315ea4e52818c9464bbd048bd6f0297e9b8c98
Aadesh <aadeshlokhande11@gmail.com>
2024-03-23 18:06:42 +0530

    23 / 03 / 2024

--- pythonScrap/Flipkart/num2wordmain.py
+++ pythonScrap/Flipkart/num2wordmain.py
@@ -1,8 +1,9 @@
-from num2words import num2words
+# from num2words import num2words
 
-file = open("abc.py","w")
-file.write("a = {\n")
-for i in range(101):
-    file.write(f"\t'{i}' : '{num2words(i)}',\n")
-file.write('}\nnum = input("enter a number = ")\nprint(a[num])')
-file.close()
+# file = open("abc.py","w")
+# file.write("a = {\n")
+# for i in range(11):
+#     file.write(f"\t'{i}' : '{num2words(i)}',\n")
+# file.write('}\nnum = input("enter a number = ")\nprint(a[num])')
+# file.close()
+
